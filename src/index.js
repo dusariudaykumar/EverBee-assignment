007490aefb6fdfed8ea1795e845ce160a403ae09
Uday Kumar <dudaykumargoud@gmail.com>
2022-07-02 15:22:06 +0530

    add: theme provider to index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import { ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { theme } from "./Components/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
