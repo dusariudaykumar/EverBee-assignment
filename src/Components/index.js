efaf275cf03addef704c28c20c5e1f7d7143a0cc
Uday Kumar <dudaykumargoud@gmail.com>
2022-07-02 15:20:20 +0530

    add : barrel exports for components

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export { GraphComponent } from "./GraphComponent";
+export { CardItem } from "./CardItem";
+export { Header } from "./Header";
+export { SideBar } from "./SideBar";
+export { TableComponent } from "./TableComponent";
